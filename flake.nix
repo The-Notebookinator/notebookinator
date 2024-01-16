683ed5768f3145ea3a166f80b3ebb824dffc4bd0
BattleCh1cken <BattleCh1cken@larkov.de>
2024-01-16 10:28:07 -0500

    🔧 Cleaned up the flake.nix file a bit

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
     typst.url = "github:typst/typst/main";
-    typstfmt.url = "github:astrale-sharp/typstfmt";
+    typstfmt.url = "github:astrale-sharp/typstfmt/0.2.7";
 
     typst.inputs.nixpkgs.follows = "nixpkgs";
     typstfmt.inputs.nixpkgs.follows = "nixpkgs";
@@ -33,24 +33,5 @@
         ];
       };
 
-      packages.default = with pkgs;
-        stdenv.mkDerivation {
-          name = "notebook";
-          postConfigurePhase = ''
-            # mkdir -p src/fonts
-            # ln -s ${font-awesome}/share/fonts/opentype/* src/fonts/
-            # ln -s ${roboto}/share/fonts/truetype/* src/fonts/
-            # ln -s ${source-sans-pro}/share/fonts/truetype/* src/fonts/
-            # ln -s ${jetbrains-mono}/share/fonts/truetype/* src/fonts/
-            # ln -s ${nerdfonts.override {fonts = ["JetBrainsMono"];}}/share/fonts/truetype/* src/fonts/
-          '';
-
-          buildCommand = ''
-            mkdir $out
-            # ${typst}/bin/typst --font-path "${self}/src/fonts" compile ${self}/main.typ $out/main.pdf
-            ${typst}/bin/typst --font-path "${nerdfonts}/share/fonts/truetype/" fonts
-            ${typst}/bin/typst --font-path "${nerdfonts}/share/fonts/truetype/"  compile ${self}/main.typ $out/main.pdf
-          '';
-        };
     });
 }
