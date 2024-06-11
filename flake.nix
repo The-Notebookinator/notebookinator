36793d96e66fc9c928c35644822915e6e6bb7762
BattleCh1cken <BattleCh1cken@larkov.de>
2024-06-11 15:13:49 -0400

    ⬆️ Upgrade the thingy

--- flake.nix
+++ flake.nix
@@ -26,6 +26,27 @@
 
         cargoHash = "sha256-KD6J8dTIPaII4ISh0PW6u1EMj5JVEDYcXuZ75ycbSys=";
       };
+      typstyle = pkgs.rustPlatform.buildRustPackage rec {
+        pname = "typstyle";
+        version = "v0.11.25";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "Enter-tainer";
+          repo = pname;
+          rev = version;
+          sha256 = "sha256-wpG+laz1k/zCnEAVOyXzrN2DOECpKWT1nVCuQUwD+p0=";
+        };
+
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
+
+
+        checkFlags = [
+          "--skip=e2e"
+        ];
+        cargoHash = "sha256-+8nadPuRKNKM+YVzZlz9rypDKjorYIwTUYkaSFrra1k=";
+      };
     in
     {
       devShell = pkgs.mkShell {
